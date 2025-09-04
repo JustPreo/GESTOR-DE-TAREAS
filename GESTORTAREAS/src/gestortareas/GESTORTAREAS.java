cb8c6764aeadf28f5ee0fc01f23588190702b2a6
JustPreo <impre0hn@gmail.com>
2025-09-04 12:32:08 -0600

    a

--- GESTORTAREAS/src/gestortareas/GESTORTAREAS.java
+++ GESTORTAREAS/src/gestortareas/GESTORTAREAS.java
@@ -19,34 +19,55 @@ public class GESTORTAREAS {
 
     static File file = new File("Tareas.txt");//Esto se va a poner en el src me imagino
     static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+    static String tareas[] = new String[10000];//Porque arraylist me bugeaba
 
     static int cTareas = 0;
 
     //El formato sera cTareas. [] nombre 
     private static void agregarT(String nombre) throws IOException {
+        tareas[cTareas] = nombre;
         cTareas++;
         FileWriter w = new FileWriter("Tareas.txt");
-        w.write(cTareas + ".[]" + nombre + "\n");// en teoria seria 1.[]Nombre
+        StringBuilder b = new StringBuilder();
+        b.append(mostrarT());
+        b.append(cTareas + ".[]" + nombre);
+        w.write(b.toString());// en teoria seria 1.[]Nombre
         w.close();
 
     }
 
-    private static void mostrarT() throws IOException {
+    private static String mostrarT() throws IOException {
+        char[] ar;
+
+        StringBuilder texto;
         FileReader r = new FileReader("Tareas.txt");
-        char[] ar = new char[(int) file.length()];
-        
+        ar = new char[(int) file.length()];
+        texto = new StringBuilder();
         r.read(ar);
-        r.close();
-        
-        
-        System.out.println("====================");
-        System.out.println(ar);
-        System.out.println("====================");
+        for (int i = 0; i < file.length(); i++) {
+            texto.append(ar[i]);
+        }
+        int c = 0;
+        int total = 0;
+        while ((c = r.read()) != -1) {
+            if (c == '\n') {
+                tareas[total++] = texto.toString();
+                texto.setLength(0);
+            } else {
+                texto.append((char) c);
+            }
+        }
+
+        return texto.toString();
 
     }
 
     //Completar tareas seria tipo conseguir el texto entero y conseguir el numero y reemplazar esos chars con eso 
-    private static void completarT() throws IOException {
+    private static void completarT(int numTarea) throws IOException {
+        String ar = mostrarT();
+        for (int i = 0; i < ar.length(); i++) {
+
+        }
 
     }
 
@@ -56,9 +77,11 @@ public class GESTORTAREAS {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
+
             while (true) {
+                System.out.println("Archivo tiene length:" + file.length());
                 initArchivo();
                 System.out.println("GESTOR DE TAREAS ");
                 System.out.println("====================");
@@ -68,33 +91,40 @@ public class GESTORTAREAS {
                 System.out.println("4. Salir");
                 System.out.println("Seleccionar una opcion:");
                 int opcion = scanner.nextInt();
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Ingresa la nueva tarea:");
-                        String nTarea = scanner.next();
-                        agregarT(nTarea);
-                        break;
-                    case 2:
-                        mostrarT();
-                        break;
-                    case 3:
-                        completarT();
-                        break;
-                    case 4:
-                        System.exit(0);
-                        break;
-                    default:
-                        break;
+                try {
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Ingresa la nueva tarea:");
+                            String nTarea = scanner.next();
+                            agregarT(nTarea);
+                            break;
+                        case 2:
+                            System.out.println(mostrarT());
+                            break;
+                        case 3:
+                            System.out.println("Ingrese la tarea a completar");
+                            int numTarea = scanner.nextInt();
+                            completarT(numTarea);
+                            break;
+                        case 4:
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.println("Opcion Invalida");
+                            break;
 
+                    }
+                } catch (IOException e) {
+                    System.out.println("Problema de archivos womp womp");
+                } catch (InputMismatchException e) {
+                    System.out.println("Ingrese un numero , no una letra o simbolo");
                 }
 
             }
-        } catch (IOException e) {
-            System.out.println("Problema de archivos womp womp");
-        } catch (InputMismatchException e) {
-            System.out.println("Ingrese un numero , no una letra o simbolo");
-        }
 
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        }
     }
 
 }
